425978523cdb4fc641432974d9e5aa2c1352bb46
jrestaino <jrestaino@gmail.com>
2021-06-06 23:59:11 -0300

    Trabajo final, ver bootstrap

--- app/public/routes.py
+++ app/public/routes.py
@@ -11,36 +11,51 @@ import datetime
 def index_trivia():
     return render_template('trivia.html')
 
+
+
 @public_bp.route('/trivia/categorias', methods=['GET'])
 @login_required
 def mostrar_categorias():
     categorias = Categoria.query.all()
+    # se agrega codigo para registrar las sesiones utilizando sesiones
     if "tiempo_inicio" not in session.keys():
         session['tiempo_inicio'] = datetime.datetime.now()
+        #session['categorias_faltantes'] = categorias
         for c in categorias:
             session[str(c.id)] = False
+
+    '''
+    mostrarCategorias = []
+    for c in categorias:
+        if not session[str(c.id)]:
+            mostrarCategorias.append(c)
+    '''
+    #return render_template('categorias.html', categorias=categorias)
     return render_template('categorias.html', categorias=categorias)
 
 
 @public_bp.route('/trivia/<id_categoria>/pregunta', methods=['GET'])
 @login_required
 def mostrar_pregunta(id_categoria):
-    preguntas = Pregunta.query.filter_by(categoria_id=id_categoria).all()
-    # elegir pregunta aleatoria pero de la categoria adecuada
-    pregunta = random.choice(preguntas)
-    categ = Categoria.query.get(id_categoria)
-
-    respuestas_posibles = pregunta.respuestas
-    return render_template('preguntas.html', categoria=categ, pregunta=pregunta, respuestas_posibles=respuestas_posibles)
+    if session[str(id_categoria)]:
+        categ = Categoria.query.get(id_categoria)
+        return render_template('categoria_respondida.html', categoria=categ)
+    else:
+        preguntas = Pregunta.query.filter_by(categoria_id=id_categoria).all()
+        # elegir pregunta aleatoria pero de la categoria adecuada
+        pregunta = random.choice(preguntas)
+        categ = Categoria.query.get(id_categoria)
+        respuestas_posibles = pregunta.respuestas
+        return render_template('preguntas.html', categoria=categ, pregunta=pregunta, respuestas_posibles=respuestas_posibles)
 
 
 @public_bp.route('/trivia/<int:id_categoria>/<int:pregunta_id>/respuesta/<int:id_respuesta>', methods=['GET'])
 @login_required
 def evaluar_respuesta(id_categoria, pregunta_id, id_respuesta):
     r = Respuesta.query.get(id_respuesta)
-    msg = "Error!!"
+    msg = "Respuesta erronea"
     if r.pregunta_id == pregunta_id and r.correcta:
-        msg = "Éxito!"
+        msg = "Respuesta existosa"
         session[str(id_categoria)] = True
 
         termina_juego = True
@@ -52,7 +67,11 @@ def evaluar_respuesta(id_categoria, pregunta_id, id_respuesta):
                 break
 
         if termina_juego:
-            tiempo_total = str(datetime.datetime.now() - session['tiempo_inicio'])
+            tiempoSegundos = datetime.datetime.now() - session['tiempo_inicio']
+            #tiempoSegundos.total_seconds()
+            #tiempo_total = str(datetime.datetime.now() - session['tiempo_inicio'])
+            tiempo_total = str(tiempoSegundos.seconds)
+
             session.clear()
             return render_template('ganador.html', tiempo_total = tiempo_total)
 
--- populate_db.py
+++ populate_db.py
@@ -15,6 +15,7 @@ with app.app_context():
     c_geogra = Categoria(descripcion="Geografía")
     c_deporte = Categoria(descripcion="Deportes")
     c_arte = Categoria(descripcion="Arte")
+    c_historia = Categoria(descripcion="Historia")
 
     # preguntas
     q_Laos = Pregunta(text="¿Cuál es la capital de Laos?", categoria = c_geogra)
@@ -29,17 +30,16 @@ with app.app_context():
 
 
     q_mundial = Pregunta(text="¿En qué país se jugó la Copa del Mundo de 1962?", categoria = c_deporte)
-    r1_Armenia = Respuesta(text="Brasil", correcta=False, pregunta=q_mundial)
-    r2_Armenia = Respuesta(text="Chile", correcta=True, pregunta=q_mundial)
-    r3_Armenia = Respuesta(text="Italia", correcta=False, pregunta=q_mundial)
+    r1_mundial = Respuesta(text="Brasil", correcta=False, pregunta=q_mundial)
+    r2_mundial = Respuesta(text="Chile", correcta=True, pregunta=q_mundial)
+    r3_mundial = Respuesta(text="Italia", correcta=False, pregunta=q_mundial)
 
     q_f1_1 = Pregunta(text="¿Quien salio campeon del mundo en 1991", categoria = c_deporte)
-    r1_f1_1 = Respuesta(text="Senna", correcta=True, pregunta=q_mundial)
-    r1_f1_1 = Respuesta(text="Prost", correcta=False, pregunta=q_mundial)
-    r1_f1_1 = Respuesta(text="Mansell", correcta=False, pregunta=q_mundial)
+    r1_f1_1 = Respuesta(text="Senna", correcta=True, pregunta=q_f1_1)
+    r2_f1_1 = Respuesta(text="Prost", correcta=False, pregunta=q_f1_1)
+    r3_f1_1 = Respuesta(text="Mansell", correcta=False, pregunta=q_f1_1)
 
     # Arte
-    c_arte = Categoria(descripcion="Arte")
     q_Gioconda = Pregunta(text="¿Quién pintó La Gioconda (Mona Lisa)?", categoria=c_arte)
     r1_Gioconda = Respuesta(text="Da Vinci", correcta=True, pregunta=q_Gioconda)
     r2_Gioconda = Respuesta(text="Dalí", correcta=False, pregunta=q_Gioconda)
@@ -49,6 +49,17 @@ with app.app_context():
     r1_Gogh = Respuesta(text="XVIII", correcta=False, pregunta=q_Gogh)
     r2_Gogh = Respuesta(text="XIX", correcta=True, pregunta=q_Gogh)
     r3_Gogh = Respuesta(text="XVII", correcta=False, pregunta=q_Gogh)
+
+    # Historia
+    q_Artigas = Pregunta(text="¿De que color era el caballo blanco de Artigas", categoria=c_historia)
+    r1_Artigas = Respuesta(text="Blanco", correcta=True, pregunta=q_Artigas)
+    r2_Artigas = Respuesta(text="Amarillo", correcta=False, pregunta=q_Artigas)
+    r3_Artigas = Respuesta(text="Negro", correcta=False, pregunta=q_Artigas)
+
+    q_Patillas = Pregunta(text="¿Quien tenía patillas más grandes?", categoria=c_historia)
+    r1_Patillas = Respuesta(text="Rivera", correcta=False, pregunta=q_Patillas)
+    r2_Patillas = Respuesta(text="Lavalleja", correcta=True, pregunta=q_Patillas)
+    r3_Patillas = Respuesta(text="Oribe", correcta=False, pregunta=q_Patillas)
 
     '''
     #Usuarios
@@ -66,20 +77,49 @@ with app.app_context():
     '''
 
     db.session.add(c_arte)
+    db.session.add(c_geogra)
+    db.session.add(c_deporte)
+    db.session.add(c_historia)
+
+    db.session.add(q_Laos)
+    db.session.add(r1_Laos)
+    db.session.add(r2_Laos)
+    db.session.add(r3_Laos)
+
+    db.session.add(q_Armenia)
+    db.session.add(r1_Armenia)
+    db.session.add(r2_Armenia)
+    db.session.add(r3_Armenia)
+
+    db.session.add(q_mundial)
+    db.session.add(r1_mundial)
+    db.session.add(r2_mundial)
+    db.session.add(r3_mundial)
+
+    db.session.add(q_f1_1)
+    db.session.add(r1_f1_1)
+    db.session.add(r2_f1_1)
+    db.session.add(r3_f1_1)
+
     db.session.add(q_Gioconda)
     db.session.add(r1_Gioconda)
     db.session.add(r2_Gioconda)
     db.session.add(r3_Gioconda)
+
     db.session.add(q_Gogh)
     db.session.add(r1_Gogh)
     db.session.add(r2_Gogh)
     db.session.add(r3_Gogh)
 
-    db.session.add(c_geogra)
-    db.session.add(c_deporte)
-    db.session.add(q_Laos)
-    db.session.add(q_Armenia)
-    db.session.add(q_mundial)
+    db.session.add(q_Artigas)
+    db.session.add(r1_Artigas)
+    db.session.add(r2_Artigas)
+    db.session.add(r3_Artigas)
+
+    db.session.add(q_Patillas)
+    db.session.add(r1_Patillas)
+    db.session.add(r2_Patillas)
+    db.session.add(r3_Patillas)
 
     '''
     db.session.add(u_u1)
